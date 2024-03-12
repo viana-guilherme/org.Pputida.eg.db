c42b6242fa8a14586b0660a978cb86e74951e1cb
Guilherme Viana <gmvsiq@gmail.com>
2024-03-12 11:35:00 -0700

    added comments for clarity

--- download-ncbi.sh
+++ download-ncbi.sh
@@ -10,7 +10,7 @@ mkdir -p resources
 baseUrl="https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Pseudomonas_putida/all_assembly_versions/GCA_000007565.2_ASM756v2/"
 filename="GCA_000007565.2_ASM756v2_feature_table.txt.gz"
 
-# downloads the file
+# downloads the feature table file
 echo "downloading ${filename}"
 curl -LO "${baseUrl}${filename}"
 gzip -dk ${filename}
@@ -18,7 +18,7 @@ gzip -dk ${filename}
 # Takes the uncompressed file to subset the information needed for the construction of the Org.db package
 featuresFile=${filename%???} # removes the final three characters of the filename string (.gz)
 
-
+# retrieving the relevant information in the feature table and save it as TSVs
 awk -F'\t' '$1 ~ "CDS" {print $17 "\t" $15 "\t" $14}' ${featuresFile} > fSym.tsv # gene symbols
 awk -F'\t' '$1 ~ "CDS" {print $17 "\t" $6}' ${featuresFile} > fChr.tsv # chromossome
 
