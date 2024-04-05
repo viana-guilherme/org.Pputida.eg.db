accc81a2b6ac4844675e0bed45c81ad626c46241
Guilherme Viana <gmvsiq@gmail.com>
2024-04-04 17:08:17 -0700

    updated relative paths to scripts

--- inst/scripts/run-workflow.R
+++ inst/scripts/run-workflow.R
@@ -1,13 +1,13 @@
 #!/usr/bin/Rscript
 
 # Run the bash script that downloads/parses the feature table from NCBI
-system2(command = "./download-ncbi.sh")
+system2(command = "./inst/scripts/download-ncbi.sh")
 
 # Runs the python scripts that downloads/parses the proteome XML file from Uniprot
-system2(command = "./download-uniprot.py")
+system2(command = "./inst/scripts/download-uniprot.py")
 
 # Finally, runs the R script that makes the org DB package
-source("makeorgdb.R")
+source("./inst/scripts/makeorgdb.R")
 
 # Compresses the resulting package directory
 zip(zipfile = "org.Pputida.eg.db.zip", files = "org.Pputida.eg.db")
